f8c0ae9ca6ff2031530683fc5d69352cfd87d200
efr-en <efrenlaris@gmail.com>
2024-12-02 19:55:16 -0500

    Created Challenges folder with 10 code war problems labeled 1 to 10, began to write down codewar challenges/solutions

---
+++ Challenges/1.js
@@ -0,0 +1,10 @@
+// Complete the square sum function so that it squares 
+// each number passed into it and then sums the results together.
+
+//this was my orignal solution baseline 
+function squareSum(numbers) {
+    numbers.forEach(number) {
+        number ** 2
+    }
+}
+//however I realized I met a block
